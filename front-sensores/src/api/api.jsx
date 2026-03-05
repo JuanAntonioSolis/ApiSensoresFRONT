6a1e2b6133dae7f97974a38a8a9104e2d304577d
JuanAntonioSolis <jsolcor457@g.educaand.es>
2026-03-05 20:36:38 +0100

    primeros pasos front

---
+++ front-sensores/src/api/api.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+import {useContext} from "react";
+import {AuthContext} from "../context/AuthContext.jsx";
+
+export const taskApi = {
+
+
+
+
+};
---
+++ front-sensores/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <p>Esto es el header</p>
+    </div>
+  )
+}
+
+export default Header
+
+
+
+
